ac43cf70d324e0b513287d61e1e37c4ac7187b97
ducpipi173 <ducvttt173@gmail.com>
2024-11-12 20:41:59 +0700

    project

---
+++ Views/introduce/contact.php
@@ -0,0 +1,90 @@
+
+<!-- pages-title-start -->
+<div class="pages-title section-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="pages-title-text text-center">
+					<h2>Thông tin liên hệ</h2>
+					<ul class="text-left">
+						<li><a href="?act=home">Trang chủ</a></li>
+						<li><span> // </span>Liên hệ</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- pages-title-end -->
+<!-- contact content section start -->
+<div class="pages contact-page section-padding">
+	<div class="container text-center">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="googleMap-info">
+					<div id="googleMap"></div>
+					<div class="map-info">
+						<p><strong>DTP Phone</strong></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-10 col-text-center">
+				<div class="contact-details">
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="single-contact">
+								<i class="mdi mdi-map-marker"></i>
+								<p>70 Tô Ký, Tân Hưng Thuận, quận 12</p>
+								<p>Trường đại học GTVT HCM</p>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="single-contact">
+								<i class="mdi mdi-phone"></i>
+								<p>(+84) 889340356</p>
+								<p>(+84) 947855789</p>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="single-contact">
+								<i class="mdi mdi-email"></i>
+								<p>2051120110@ut.edu.vn</p>
+								<p>2051120115@ut.edu.vn</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- contact content section end -->
+<!-- Google Map JS -->
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script>
+	function initialize() {
+	
+		var mapOptions = {
+		zoom: 17,
+		hue: '#E9E5DC',
+		scrollwheel: false,
+		mapTypeId:google.maps.MapTypeId.TERRAIN,
+		center: new google.maps.LatLng(10.8657045,106.6156012)
+		};
+
+		var map = new google.maps.Map(document.getElementById('googleMap'),
+			mapOptions);
+
+
+		var marker = new google.maps.Marker({
+		position: map.getCenter(),
+		icon: 'public/img/map-marker.png',
+		map: map
+		});
+
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+</script>
